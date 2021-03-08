50ccd2a5361818ab65c4b7b55fd9dc84f5aee534
nebrocks <nebalgos.com>
2021-03-08 20:17:05 +0530

    modified Test.java

--- Test.java
+++ Test.java
@@ -2,5 +2,7 @@ public class Test{
 	public void test(){
 		System.out.println("added test line.");
 		System.out.println("added test line.");
+		
+		System.out.println("line has to be stashed");
 	}
 }
